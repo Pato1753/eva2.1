83c84c819e3cb57f7c6b685f0e63023ec58c1e53
pato1753 <patricio.ollivares1753@gmail.com>
2025-05-26 14:33:20 -0400

    update js

--- js/script2.js
+++ js/script2.js
@@ -1,5 +1,4 @@
 //validar nombre, debe contener solo letras y no ser vacio
-//validar nombre, debe contener solo letras y no ser vacio
 let personas = []
 
 function validar(){
@@ -11,25 +10,52 @@ function validar(){
     //let eErrorEdad = 
     let valNombre = valLetras(eNombre, vNombre, eErrorNombre)
     let valEdad = valEdad(eEdad, vEdad, eErrorEdad)
-}
+    if (valNombre == "bueno" && valEdad == "bueno"){
+        let p = {
+            Nombre : vNombre,
+            Edad : vEdad,
+        }
+        personas.push(p)
+        eNombre.style.backgroundColor = "green"
+        eNombre.style.Color = "white"
+    }else if(valNombre =="Malo"){
+        alert("Ta malo")
+        eNombre.style.backgroundColor = "red"
+        eNombre.style.Color = "white"
+    }
+
+    }
+
 
 function valLetras(elemento, valor, eError){
-    for(let i = 0;valor;++){
-        if(i in ["1","2","3","4","5","6","7","8","9","0"]){
-            console.log("Error, hay un valor numerico en el nombre")
+    // for(let i = 0;valor;++){
+    //     if(i in ["1","2","3","4","5","6","7","8","9","0"]){
+    //         console.log("Error, hay un valor numerico en el nombre")
+                // return "malo"
 
-        }
+    //     }
+    // }
+    if (valor == none){
+        alert("Debe ingresar informacion, el campo esta vacio")
+        console.log("No hay campos")
+        return "malo"
+    }else{
+        return "bueno"
     }
 }
 
 function valEdad(elemento, valor, eError){
-    
+
 
 }
 
 function cargarDatos(){
-    let info
+    let contador = 0
+    for(let i = 0;personas[contador],++){
+        innerHTML = ""                    //VER EN CASA --------------------------------------------------------------------------
+        
+        contador++
+    }
 }
 
-//validar edad: Debe ser mayor o igual a 18 y menor a 100
 //validar edad: Debe ser mayor o igual a 18 y menor a 100
