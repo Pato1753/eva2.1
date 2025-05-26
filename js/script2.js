3b5bc87eded35b559a520e94739d95260b0bd7ff
pato1753 <patricio.ollivares1753@gmail.com>
2025-05-26 14:10:16 -0400

    comit2

--- js/script2.js
+++ js/script2.js
@@ -1,2 +1,30 @@
 //validar nombre, debe contener solo letras y no ser vacio
+//validar nombre, debe contener solo letras y no ser vacio
+let personas = []
+
+function validar(){
+    let eNombre = document.getElementById("nombre")
+    let vNombre = eNombre.value
+    //let eErrorNombre = 
+    let eEdad = document.getElementById("edad")
+    let vEdad = eEdad.value
+    //let eErrorEdad = 
+    let valNombre = valLetras(eNombre, vNombre, eErrorNombre)
+    let valEdad = valEdad(eEdad, vEdad, eErrorEdad)
+}
+
+function valLetras(elemento, valor, eError){
+    for(let)
+    if (valor == Number)
+}
+
+function valEdad(elemento, valor, eError){
+
+}
+
+function cargarDatos(){
+    let info
+}
+
+//validar edad: Debe ser mayor o igual a 18 y menor a 100
 //validar edad: Debe ser mayor o igual a 18 y menor a 100
